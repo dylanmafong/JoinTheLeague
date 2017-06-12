0bdf4625526abd57587a7fa0cff7d55455c2e1ae
stevotennis <stevotennis@gmail.com>
2017-06-11 20:45:23 -0700

    6-12
    added exercises for 06/12

---
+++ src/for-loop/Minion.java
@@ -0,0 +1,25 @@
+/*
+ * Minion.java
+ *
+ * You met a minion who is banana for fruits (no puns intended...)! Ask them for 
+ * their name and they will yell a kind of fruit x times, where x is a random 
+ * number between 1 and 20. But if you ended up asking Gru what his favorite 
+ * fruit, he would say, "I don't like fruits!".
+ * 
+ *  
+ * 1. Ask your minion what their name is? (hint: use JOptionPane)
+ * 2. Ask your minion what their favorite fruit is?
+ * 3. Get a number between 1 and 20.
+ * 4. Make the minion go crazy and say their favorite fruit names over and over!
+ * 5. But remember that Gru doesn't like fruits!
+ */
+
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+public class Minion {
+
+	public static void main(String[] args) {
+
+	}
+}
